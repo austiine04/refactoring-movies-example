0bbccc458b0a98338fcc15242f5b49c22c4378f7
austiine <austiine04@gmail.com>
2015-07-12 23:55:43 +0300

    adding more tests

--- tests/test_customer.rb
+++ tests/test_customer.rb
@@ -35,6 +35,36 @@ class CustomerTest < MiniTest::Test
     assert_equal customer.statement, result
   end
 
+  def test_single_three_day_rental_for_regular_movie
+    result = result_string "John Doe", 3.5, 3.5, 1
+    movie = Movie.new "XYZ", Movie::REGULAR
+    rental = Rental.new movie, 3
+
+    customer = create_customer("John Doe", rental)
+
+    assert_equal result, customer.statement
+  end
+
+  def test_single_three_day_rental_for_new_release
+    result = result_string "John Doe", 9, 9, 2
+    movie = Movie.new "XYZ", Movie::NEW_RELEASE
+    rental = Rental.new movie, 3
+
+    customer = create_customer("John Doe", rental)
+
+    assert_equal result, customer.statement
+  end
+
+  def test_single_four_day_rental_for_childrenz_movie
+    result = result_string "John Doe", 3.0, 3.0, 1
+    movie = Movie.new "XYZ", Movie::CHILDREN
+    rental = Rental.new movie, 4
+
+    customer = create_customer("John Doe", rental)
+
+    assert_equal result, customer.statement
+  end
+
   private
 
   def create_customer name, rental
