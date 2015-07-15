dbe2ef9970f69163f113111f9c90dd3fbc75479e
austiine <austiine04@gmail.com>
2015-07-15 20:11:27 +0300

    move charge and frewuent renter points to movie as they violate tell dont ask rule

--- lib/movie.rb
+++ lib/movie.rb
@@ -9,4 +9,23 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge days_rented
+    amount = 0
+    case @price_code
+    when Movie::REGULAR
+      amount += 2
+      amount += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      amount += days_rented * 3
+    when Movie::CHILDREN
+      amount += 1.5
+      amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    amount
+  end
+
+  def frequent_renter_points days_rented
+    (@price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 end
--- lib/rental.rb
+++ lib/rental.rb
@@ -6,21 +6,10 @@ class Rental
   end
 
   def charge
-    amount = 0
-    case @movie.price_code
-    when Movie::REGULAR
-      amount += 2
-      amount += (@days_rented - 2) * 1.5 if @days_rented > 2
-    when Movie::NEW_RELEASE
-      amount += @days_rented * 3
-    when Movie::CHILDREN
-      amount += 1.5
-      amount += (@days_rented - 3) * 1.5 if @days_rented > 3
-    end
-    amount
+    @movie.charge @days_rented
   end
 
   def frequent_renter_points
-    (@movie.price_code == Movie::NEW_RELEASE && @days_rented > 1) ? 2 : 1
+    @movie.frequent_renter_points @days_rented
   end
 end
